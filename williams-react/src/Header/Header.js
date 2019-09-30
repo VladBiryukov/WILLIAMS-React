bd71008f582a89eab241e8f5f6246e28a0708c16
Vlad <ksand.1@ya.ru>
2019-09-30 22:12:53 +0400

    готов компонент navigation, приступил к header

---
+++ williams-react/src/Header/Header.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Navigation from '../Navigation'
+
+class Header extends React.Component {
+    
+}
---
+++ williams-react/src/Navigation/Navigation.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import '../index.css'
+
+
+class Navigation extends React.Component {
+   state = {
+      nav: [
+         { href: "#", text: "Who We Are" },
+         { href: "#", text: "What We Do" },
+         { href: "#", text: "Patenting Process" },
+         { href: "#", text: "FAQ" },
+         { href: "#", text: "Fee Structure" },
+         { href: "#", text: "Contact" },
+         { href: "#", text: "Blog" },
+      ]
+   }
+   renderNav() {
+      return this.state.nav.map(nav => {
+         return (
+            <a className="nav__link" href={nav.href} key={nav.text}>{nav.text}</a>
+         )
+      })
+   }
+   render() {
+      return (
+         <nav className='nav'>
+            {this.renderNav()}
+         </nav>
+      )
+   }
+}
+export default Navigation
