cfb119a43ee6fd4f62d0d34af5338b243bb9404f
Vlad <ksand.1@ya.ru>
2019-09-30 22:17:40 +0400

    готов компонент navigation, приступил к header

--- williams-react/src/App.js
+++ williams-react/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Navigation from './Navigation/Navigation';
+import Header from './Header/Header'
 function App() {
   return (
     <div className="App">
-     
+      <Header />
     </div>
   );
 }
--- williams-react/src/Header/Header.js
+++ williams-react/src/Header/Header.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import Navigation from '../Navigation'
+import Navigation from '../Navigation/Navigation'
 
 class Header extends React.Component {
-    
+    render() {
+        return (
+            <Navigation />
+        )
+    }
 }
+export default Header
